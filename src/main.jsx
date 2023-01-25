84ca847ed3f3c92a704e8b38fbad2e5b58ac61a1
Eduardo <esainz@paradigmadigital.com>
2023-01-25 21:14:31 +0100

    adding dist

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App'
 import './index.css'
+import ErrorPage from "./error-page";
+import Contact, {
+  loader as contactLoader,
+} from "./routes/contact";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import EditContact, {
+  action as editAction,
+} from "./routes/edit";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+    ],
+  },
+ 
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
